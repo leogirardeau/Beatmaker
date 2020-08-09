6d6f0c2ad5e971b850dcb56f69a0e018402cf37e
leogirardeau <leogir.pro@gmail.com>
2020-08-09 07:55:11 +0200

    removed bubble child in function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
-    const visual = document.querySelectorAll(".visual");
+    const visual = document.querySelector(".visual");
     const colors = [
         "#60d394",
         "#d36060",
@@ -27,5 +27,8 @@ window.addEventListener("load", () => {
         visual.appendChild(bubble);
         bubble.style.backgroundColor = colors[index];
         bubble.style.animation = "jump 1s ease";
-    }
+        bubble.addEventListener("animationend", function () {
+            visual.removeChild(this);
+        });
+    };
 });
