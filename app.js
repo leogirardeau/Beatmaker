39533027a1895ef0239a7b61588042cb510a28dd
leogirardeau <leogir.pro@gmail.com>
2020-08-09 07:29:50 +0200

    sounds click function for each pads

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     const pads = document.querySelectorAll(".pads div");
+
+    //Lets get going with the sound here
+    pads.forEach((pad, index) => {
+        pad.addEventListener("click", function () {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+        });
+    });
 });
